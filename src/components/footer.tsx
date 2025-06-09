2eddd60c4a59557ebb3a3c3b653808f6b5a0fe18
User <user@example.com>
2025-06-09 22:23:01 +0800

    🎨 个性化版权信息：改为 'News by CY'

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,11 @@
+import { Homepage } from "@shared/consts"
+
 export function Footer() {
   return (
     <>
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
-        <span>New by CY</span>
+        <span>News by CY</span>
       </span>
     </>
   )
